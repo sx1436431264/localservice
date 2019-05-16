331f9db9990e81e36c508c6e3670cf4c8e4dfa81
Eric Simons <ericsimons@erics-mac-mini.lan>
2019-05-16 12:41:10 -0700

    latest

--- scripts/_copyhtml.js
+++ scripts/_copyhtml.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+var path = require('path');
 var minify = require('html-minifier').minify;
 
 function copyAndMinify(path, destination) {
@@ -13,8 +14,28 @@ function copyAndMinify(path, destination) {
   });
 }
 
+// function ensurePathExists(filePath) {
+//   var dirname = path.dirname(filePath);
+//   console.log(dirname)
+//   if (fs.existsSync(dirname)) {
+//     return true;
+//   }
+//   fs.mkdirSync(dirname, { recursive: true });
+// }
+
+function scaffoldDirs() {
+  function ensureDirExists(path) {
+    if (fs.existsSync(path)) {
+      return true;
+    }
+    fs.mkdirSync(path, { recursive: true });
+  }
+  
+  ensureDirExists('./dist/server');
+}
+
 module.exports = function() {
   const fs = require('fs');
-
-  copyAndMinify('./src/index.html', './dist/index.html');
+  scaffoldDirs();
+  copyAndMinify('./src/index.html', './dist/server/index.html');
 }
